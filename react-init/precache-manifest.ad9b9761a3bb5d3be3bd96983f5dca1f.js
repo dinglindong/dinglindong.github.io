8bdcf325f6e27c790a85f28e0e29d3a10ab08ef3
dinglindong <893808866@qq.com>
2019-07-06 14:10:53 +0800

    add react-init

---
+++ react-init/precache-manifest.ad9b9761a3bb5d3be3bd96983f5dca1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f6a1be446a49d61e5312a1ba36b5cd",
+    "url": "/react-init/index.html"
+  },
+  {
+    "revision": "7d48eb4c8c6237821ce8",
+    "url": "/react-init/static/css/main.c81e3cdd.chunk.css"
+  },
+  {
+    "revision": "e3c6d82223bb5ac36e5c",
+    "url": "/react-init/static/js/2.3476c657.chunk.js"
+  },
+  {
+    "revision": "7d48eb4c8c6237821ce8",
+    "url": "/react-init/static/js/main.9f5d49de.chunk.js"
+  },
+  {
+    "revision": "dcf24fb6c73866fff05a",
+    "url": "/react-init/static/js/runtime~main.cbe1f420.js"
+  },
+  {
+    "revision": "5c25e1cb9735adf1b556f42b8df9b7d1",
+    "url": "/react-init/static/media/iconfont.5c25e1cb.svg"
+  },
+  {
+    "revision": "7e008a771d5079e5051b632adcff1243",
+    "url": "/react-init/static/media/iconfont.7e008a77.eot"
+  },
+  {
+    "revision": "d828102abc66b0a0a7cd5eb3f9b4e2b4",
+    "url": "/react-init/static/media/iconfont.d828102a.ttf"
+  }
+]);
