73de16ccd953eeb3c7a07e3bb8ff82f5782338bd
dinglindong <lindong.ding@pactera.com>
2021-10-14 12:23:33 +0800

    update react project

---
+++ react-antd-admin/precache-manifest.cd30bcd7ec0c023f1383999f30092aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea97b38d8c1bad5f06e3b0bbb0f47016",
+    "url": "/react-antd-admin/index.html"
+  },
+  {
+    "revision": "8f5f2017339836d0fdde",
+    "url": "/react-antd-admin/static/css/2.bfe58d8b.chunk.css"
+  },
+  {
+    "revision": "8664861c055233539b14",
+    "url": "/react-antd-admin/static/css/main.195023b5.chunk.css"
+  },
+  {
+    "revision": "8f5f2017339836d0fdde",
+    "url": "/react-antd-admin/static/js/2.e3ac74f3.chunk.js"
+  },
+  {
+    "revision": "8664861c055233539b14",
+    "url": "/react-antd-admin/static/js/main.26638d86.chunk.js"
+  },
+  {
+    "revision": "7dd1197cf66b85d075b0",
+    "url": "/react-antd-admin/static/js/runtime~main.b895a58c.js"
+  }
+]);
